d1b0e61ce7e33f27c0b14ae67c7e135015f40f6a
Ronald Harsh <ronald.harsh@protonmail.com>
2024-07-28 08:41:50 -0500

    Started day 1

---
+++ string_manipulation_code_intelligence.py
@@ -0,0 +1,7 @@
+# Using three print statements
+print("Hello world!")
+print("Hello world!")
+print("Hello world!")
+
+# Using a single print statement with new-line escape character
+print("Hello world!\nHello world!\nHello world!")
