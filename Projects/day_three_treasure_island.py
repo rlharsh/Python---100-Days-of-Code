2007b2090abbef5c2a24a20d14ffdafcd16d3171
Ronald Harsh <ronald.harsh@protonmail.com>
2024-07-30 10:35:26 -0500

    Created the Treasure Island adventure.

---
+++ Projects/day_three_treasure_island.py
@@ -0,0 +1,50 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+death_reason = ""
+choice_one= input("Do you go left or right?: ").lower()
+
+if choice_one == "left":
+    choice_two = input("You\'ve come to a lake, shall you swim, or do you wait?: ").lower()
+    if choice_two == "wait":
+        choice_three = input("You happen upclearon a door, behold, it has three switches, each marked by a color, Red, Yellow, and Blue, which button switch shall you pull?: ").lower()
+        if choice_three == "red":
+            death_reason = "Burned by fire."
+        elif choice_three == "blue":
+            death_reason = "Eaten by beasts."
+        elif choice_three == "yellow":
+            print("You Win!")
+        else:
+            death_reason = "You have faded into the unknown."
+    else:
+        death_reason = "Attacked by trout."
+else:
+    death_reason = "Fall into a hole."
+
+if death_reason != "":
+    print(f"{death_reason} Game Over.")
